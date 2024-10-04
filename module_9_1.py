ae913d846c23a9fd1be25634ee7abf3bdddf068d
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-10-04 07:25:42 +0300

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -6,25 +6,5 @@ def apply_all_func(int_list, *functions):
     return result
 
 
-def min_(int_list):
-    return min(int_list)
-
-
-def max_(int_list):
-    return max(int_list)
-
-
-def len_(int_list):
-    return len(int_list)
-
-
-def sum_(int_list):
-    return sum(int_list)
-
-
-def sorted_(int_list):
-    return sorted(int_list)
-
-
-print(apply_all_func([6, 20, 15, 9], min_, max_))
+print(apply_all_func([6, 20, 15, 9], min, max))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
