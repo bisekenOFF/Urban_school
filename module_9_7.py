8d87a84af0239c166bbacde4a31660a660c4be03
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-10-15 19:29:44 +0400

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        num = func(a, b, c)
+        div = 0
+        for i in range(2, num):
+            if num % i == 0:
+                div += 1
+        if div == 0:
+            print("Простое число")
+        else:
+            print("Составное число")
+        return num
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
