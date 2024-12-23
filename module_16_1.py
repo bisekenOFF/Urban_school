99c006559c95658e647047070c782f9cd9fd818d
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-12-23 22:04:23 +0400

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/user/admin")
+async def admin_page() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{username:str}/{age:int}")
+async def user_page(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}. Возраст: {age}"}
+
+
+@app.get("/user/{user_id}")
+async def user_page(user_id: int) -> dict:
+    return {"message": f"Вы вошли как пользователь №{user_id}"}
+
+
+@app.get("/")
+async def general_page() -> dict:
+    return {"message": "Главная страница"}
