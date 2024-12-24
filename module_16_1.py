d144170e61aec270555032ce1445b257a98a477f
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-12-24 08:42:02 +0400

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -5,19 +5,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/user/admin")
-async def admin_page() -> dict:
+async def admin_page() -> str:
     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{username:str}/{age:int}")
-async def user_page(username: str, age: int) -> dict:
+async def user_page(username: str, age: int) -> str:
     return {"message": f"Информация о пользователе. Имя: {username}. Возраст: {age}"}
 
 
 @app.get("/user/{user_id}")
-async def user_page(user_id: int) -> dict:
+async def user_page(user_id: int) -> str:
     return {"message": f"Вы вошли как пользователь №{user_id}"}
 
 
 @app.get("/")
-async def general_page() -> dict:
+async def general_page() -> str:
     return {"message": "Главная страница"}
