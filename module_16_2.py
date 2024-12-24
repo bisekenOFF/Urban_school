aa3a57fe5d3bb5d17f3fd18070fa5c3aad520544
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-12-24 08:55:09 +0400

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,6 +1,7 @@
 #Переименуйте файл на main.py
 
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -12,14 +13,15 @@ async def general_page() -> str:
 async def admin_page() -> str:
     return {"message": "Вы вошли как администратор"}
 
-
-@app.get("/user/{username:str}/{age:int}")
-async def user_page(username: str = Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')
-                    , age: int = Path(ge=18, le=120, description="Enter age", example='24')) -> str:
+@app.get("/user/{username}/{age}")
+async def user_page(
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='24')]
+) -> str:
     return {"message": f"Информация о пользователе. Имя: {username}. Возраст: {age}"}
 
-
 @app.get("/user/{user_id}")
-async def user_page(user_id: int = Path(ge=1, le=100, description="Enter User ID", example='1')) -> str:
+async def user_page(
+    user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example='1')]
+) -> str:
     return {"message": f"Вы вошли как пользователь №{user_id}"}
-
