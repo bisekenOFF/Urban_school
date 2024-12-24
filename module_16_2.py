2625dab3a94fe9fd45ee95355f7e80dfeb9b31c3
bisekenOFF <142386055+bisekenOFF@users.noreply.github.com>
2024-12-24 11:32:03 +0400

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -7,21 +7,21 @@ app = FastAPI()
 
 @app.get("/")
 async def general_page() -> str:
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 @app.get("/user/admin")
 async def admin_page() -> str:
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 @app.get("/user/{username}/{age}")
 async def user_page(
     username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='24')]
 ) -> str:
-    return {"message": f"Информация о пользователе. Имя: {username}. Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}. Возраст: {age}"
 
 @app.get("/user/{user_id}")
 async def user_page(
     user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example='1')]
 ) -> str:
-    return {"message": f"Вы вошли как пользователь №{user_id}"}
+    return f"Вы вошли как пользователь №{user_id}"
